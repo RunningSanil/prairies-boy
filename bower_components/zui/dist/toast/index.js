f72d03b759fc6252c1ee58a6037bd50e4660ef9f
RunningSnail <363869244@qq.com>
2017-01-14 17:49:21 +0800

    update coding style

--- bower_components/zui/dist/toast/index.js
+++ bower_components/zui/dist/toast/index.js
@@ -1,1 +1,24 @@
-module.exports={showZuiToast(a,b){var c=this.data.zuiToast||{};clearTimeout(c.timer),c={show:!0,title:a},this.setData({zuiToast:c}),c.timer=setTimeout(()=>{this.clearZuiToast()},b||3e3)},clearZuiToast(){var a=this.data.zuiToast||{};clearTimeout(a.timer),this.setData({'zuiToast.show':!1})}};
+module.exports = {
+    showZuiToast(a, b) {
+        var c = this.data.zuiToast || {};
+        clearTimeout(c.timer),
+        c = {
+            show: !0,
+            title: a
+        },
+        this.setData({
+            zuiToast: c
+        }),
+        c.timer = setTimeout(() => {
+            this.clearZuiToast()
+        },
+        b || 3e3)
+    },
+    clearZuiToast() {
+        var a = this.data.zuiToast || {};
+        clearTimeout(a.timer),
+        this.setData({
+            'zuiToast.show': !1
+        })
+    }
+};
